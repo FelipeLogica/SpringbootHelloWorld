a981d0478f2efe44f959a61ad654c8385f55abaa
FelipeLogica <rasenvoigt@gmail.com>
2020-06-14 17:21:07 -0300

    Simples hello world com Springboot

---
+++ src/main/java/Voigt/Felipe/ExercicioHeloWord/ExercicioHeloWordApplication.java
@@ -0,0 +1,16 @@
+package Voigt.Felipe.ExercicioHeloWord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioHeloWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioHeloWordApplication.class, args);
+
+		System.out.println("\noi mundo ! (Exercicio em Gradle.)");
+
+	}
+
+}
---
+++ src/test/java/Voigt/Felipe/ExercicioHeloWord/ExercicioHeloWordApplicationTests.java
@@ -0,0 +1,13 @@
+package Voigt.Felipe.ExercicioHeloWord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioHeloWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
